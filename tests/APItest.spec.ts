25073896928669d19797bdf103b69306d18664b2
Olena Kateryniuk <lena.katerynyuk@gmail.com>
2024-07-12 09:27:47 +0300

    fix test

--- tests/APItest.spec.ts
+++ tests/APItest.spec.ts
@@ -11,20 +11,20 @@ test.describe('API Tests', () => {
 
   test('Correct email and name in me request', async ({ api }) => {
     const { name, email } = await api.get('me')
-    await expect(name).toBe('Test User'); 
-    await expect(email).toBe(process.env.EMAIL);
+    expect(name).toBe('Test User'); 
+    expect(email).toBe(process.env.EMAIL);
   })
 
   test('Correct number of drivers in a table', async ({ page, api }) => {
     const driversInTable = await page.locator('[class="v-data-table__tr"]').count();
     const { items } = await api.get('drivers')
-    await expect(items.length).toBe(driversInTable);
+    expect(items.length).toBe(driversInTable);
   })
 
   test('Correct number of trucks in a table', async ({ page, truckPage, api }) => {
     await goto(truckPage);
     const trucksInTable = await page.locator('[class="v-data-table__tr"]').count();
     const { items } = await api.get('trucks');
-    await expect(items.length).toBe(trucksInTable);
+    expect(items.length).toBe(trucksInTable);
   })
 })
